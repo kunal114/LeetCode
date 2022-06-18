b0ad39c92194fd423ea19325faa14ac5fe40adcd
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-06-18 14:39:59 +0530

    Time: 120 ms (10.62%), Space: 19.7 MB (5.98%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -13,33 +13,18 @@ public:
         ListNode *temp2 = headB;
         unordered_set<ListNode*> s;
         
-        while(temp1!=NULL && temp2!=NULL){
-            if(temp1==temp2){
-                return temp1;
-            }
-            else if(s.find(temp1)!=s.end()){
-                return temp1;
-            }
-            else if(s.find(temp2)!=s.end()){
-                return temp2;
-            }
-            else{
-                s.insert(temp1);
-                s.insert(temp2);
-                temp1=temp1->next;
-                temp2=temp2->next;
-            }
-        }
         while(temp1!=NULL){
             if(s.find(temp1)!=s.end()){
                 return temp1;
             }
+            s.insert(temp1);
             temp1=temp1->next;
         }
         while(temp2!=NULL){
             if(s.find(temp2)!=s.end()){
                 return temp2;
             }
+            s.insert(temp2);
             temp2=temp2->next;
         }
         return NULL;
