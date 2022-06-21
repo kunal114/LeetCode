e5a6861f7ba314946c3947cf46be88d64dfd7d25
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-06-21 23:34:57 +0530

    Time: 5 ms (55.99%), Space: 8 MB (42.62%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rec(vector<int> nums,int i,vector<int> v,vector<vector<int>>&ans){
+        ans.push_back(v);
+        for(int ind=i;ind<nums.size();ind++){
+            if(ind>i and nums[ind]==nums[ind-1]) continue;
+            v.push_back(nums[ind]);
+            rec(nums,ind+1,v,ans);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> v;
+        rec(nums,0,v,ans);
+        return ans;
+    }
+};
