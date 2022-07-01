99e9eab0009d469c7f86159878218cce0c2df9cd
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-02 00:30:47 +0530

    Time: 19 ms (32.81%), Space: 7.2 MB (29.95%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        if(n==2) return "11";
+        
+        string s = "11";
+        
+        for(int i=3;i<=n;i++){
+            string temp="";
+            s=s+'&';//to consider last element of the string in the count
+            int cnt=1;
+            
+            for(int j=1;j<s.size();j++){
+                if(s[j]==s[j-1]){
+                    cnt++;
+                }
+                else{
+                    temp+=to_string(cnt)+s[j-1];
+                    cnt=1;
+                }
+            }
+            
+            s=temp;
+        }
+        
+        return s;
+    }
+};
