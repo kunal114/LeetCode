e7df8da06821c7289281045db23a798a2a6f280b
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-02 01:24:06 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (24.50%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> computeLPSArray(string needle) {
+        int m = needle.size();
+        vector<int> lps(m);
+        lps[0] = 0;
+
+        int i = 1;
+        int len = 0;
+
+        while(i < m) {
+            if(needle[i] == needle[len]) {
+                lps[i] = len + 1;
+                len++;
+                i++;
+            } else {
+                if(len == 0) {
+                    i++;
+                } else {
+                    len = lps[len - 1];
+                }
+            }
+        }
+
+        return lps;
+    }
+    int kmp(string haystack, string needle){
+        vector<int> lps = computeLPSArray(needle);
+
+        int n = haystack.size(), m = needle.size();
+
+        int i = 0;
+        int j = 0;
+
+        while(i < n) {
+            if(haystack[i] == needle[j]) {
+                i++, j++;
+            } else {
+                if(j == 0) {
+                    i++;
+                } else {
+                    j = lps[j-1];
+                }
+            }
+            if(j == m) {
+                return i - j;
+            }
+        }
+
+        return -1;
+    }
+    int strStr(string haystack, string needle) {
+        
+        int ans = kmp(haystack, needle);        
+        // int ans = RabinKarp(string haystack, string needle);
+        // int ans = Z_algo(string haystack, string needle);
+        
+        return ans;
+
+    }
+};
