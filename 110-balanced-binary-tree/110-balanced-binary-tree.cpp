30a3fb0f1f6007896afc1168cb07adf8ec101434
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-04 15:21:17 +0530

    Time: 26 ms (28.22%), Space: 20.9 MB (54.10%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -15,22 +15,19 @@ public:
         if(root==NULL) return 0;
         
         int lh = height(root->left);
+        if(lh==-1){
+            return -1;
+        }
+        
         int rh = height(root->right);
+        if(rh==-1){
+            return -1;
+        }
         
+        if(abs(lh-rh)>1) return -1;
         return max(lh,rh)+1;
-        
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
-        
-        //at each height we have to check for balance(start from the leaf node)
-        if(!isBalanced(root->left)) return false;
-        if(!isBalanced(root->right)) return false;
-        
-        int left = height(root->left);
-        int right = height(root->right);
-        
-        if(abs(left-right)<=1) return true;
-        else return false;
+        return (height(root)!=-1);
     }
 };
