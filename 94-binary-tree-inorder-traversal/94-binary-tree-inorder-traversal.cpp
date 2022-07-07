ebc299815db8a82783024f665d3ac23c3b2cc9e5
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-07 21:20:54 +0530

    Time: 3 ms (60.33%), Space: 8.4 MB (75.22%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -19,9 +19,39 @@ public:
         ans.push_back(root->val);
         inorder(root->right,ans);
     }
+    void morris(TreeNode* root,vector<int> &ans){
+        if(root==NULL) return;
+        while(root){
+            if(root->left==NULL){
+                ans.push_back(root->val);
+                root=root->right;
+            }
+            else{
+                //find inorder predecessor
+                TreeNode *curr = root->left;
+                while(curr->right && curr->right!=root){
+                    curr=curr->right;
+                }
+                
+                //if predecessor already points to root, it means its already visited
+                if(curr->right==root){
+                    curr->right=NULL;
+                    ans.push_back(root->val);
+                    root=root->right;
+                }//else traverse the left subtree of root
+                else{
+                    curr->right = root;
+                    root=root->left;
+                    
+                }
+            }
+        }
+    }
     vector<int> inorderTraversal(TreeNode* root) {
+        //if we are stack(push right node first then left)
         vector<int> ans;
-        inorder(root, ans);
+        // inorder(root, ans);
+        morris(root,ans);
         return ans;
     }
 };
