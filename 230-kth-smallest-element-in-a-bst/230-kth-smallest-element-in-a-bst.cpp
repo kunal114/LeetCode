6a483397527f510216a3a95a659d62d490a96507
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-08 00:50:28 +0530

    Time: 35 ms (28.61%), Space: 24.1 MB (86.44%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -9,39 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    void morris(TreeNode* root,int k,vector<int> &ans){
+    int count = 0;
+    void inorder(TreeNode *root,int k,int &ans){
         if(root==NULL) return;
-        while(root){
-            if(root->left==NULL){
-                ans.push_back(root->val);
-                root=root->right;
-            }
-            else{
-                //find inorder predecessor
-                TreeNode *curr = root->left;
-                while(curr->right && curr->right!=root){
-                    curr=curr->right;
-                }
-                
-                //if predecessor already points to root, it means its already visited
-                if(curr->right==root){
-                    curr->right=NULL;
-                    ans.push_back(root->val);
-                    root=root->right;
-                }//else traverse the left subtree of root
-                else{
-                    curr->right = root;
-                    root=root->left;
-                    
-                }
-            }
+        
+        inorder(root->left,k,ans);
+        
+        count++;
+        if(k==count){
+            ans = root->val;
+            return;
         }
+        inorder(root->right,k,ans);
+        
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> ans;
-        morris(root,k,ans);
-        return ans[k-1];
+        int ans;
+        inorder(root,k,ans);
+        return ans;
     }
 };
