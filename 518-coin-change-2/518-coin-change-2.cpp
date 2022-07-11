95e7db0f7379629e289cde3e1d1c3be7742caa06
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-11 20:19:27 +0530

    Time: 27 ms (63.81%), Space: 12 MB (61.64%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -13,12 +13,14 @@ public:
         for(int i=1;i<n+1;i++){
             for(int j=1;j<amount+1;j++){
                 if(coins[i-1]<=j){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    dp[i][j] = dp[i][j-coins[i-1]]+dp[i-1][j];
                 }
-                else
+                else{
                     dp[i][j] = dp[i-1][j];
+                }
             }
         }
+        
         return dp[n][amount];
     }
 };
