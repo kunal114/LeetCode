94f84634436444b4e5f26809dcff1e527af3da28
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-11 15:33:24 +0530

    Time: 4 ms (97.62%), Space: 7.2 MB (84.17%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,32 +1,25 @@
-// #include<bits/stdc++.h>
 class Solution {
 public:
-    int minDistance(string s, string t) {
-        int n1 = s.length(), n2 = t.length();
-        int dp[n1+1][n2+1];
-        if(n1==0 and n2!=0 || n1!=0 and n2==0){
-            return 1;
-        }
+    int minDistance(string word1, string word2) {
+        int m=word1.size(),n=word2.size();
+        int dp[m+1][n+1];
         
-        for(int j=0;j<n2+1;j++){
-            dp[0][j] = j ;
-        }
-        
-        for(int i=0;i<n1+1;i++){
+        for(int i=0;i<m+1;i++){
             dp[i][0] = i;
         }
-        
-        for(int i=1;i<n1+1;i++){
-            for(int j=1;j<n2+1;j++){
-                if(s[i-1]==t[j-1]){
+        for(int j=0;j<n+1;j++){
+            dp[0][j] = j;
+        }
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(word1[i-1]==word2[j-1]){
                     dp[i][j] = dp[i-1][j-1];
                 }
-                
                 else{
-                    dp[i][j] = min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+                    dp[i][j] = min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
                 }
             }
         }
-        return dp[n1][n2];
+        return dp[m][n];
     }
 };
