e788c5d74c0c0ec65c9a07953ccaca57973354b8
kunal114 <89271357+kunal114@users.noreply.github.com>
2022-07-12 23:44:35 +0530

    Time: 43 ms (83.05%), Space: 10.2 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -4,19 +4,22 @@ public:
     bool dfs(vector<int>& matchsticks,vector<int> &sides, int side,int ind){
         if(ind==matchsticks.size()){
             if(sides[0]==sides[1] and sides[1]==sides[2] and sides[2]==sides[3])
-                return true;
-            
+                return true;  
             return false;
         }
         
         for(int i=0;i<4;i++){
             if(sides[i]+matchsticks[ind]>side) continue; //dont add
-            // int j=i-1;
-            // while(j>=0){
-            //     if(sides[j]==sides[i]) break;
-            //     j--;
-            // }
-            // if(j!=-1) continue;
+            
+            //OPTIMIZATION
+            int j=i-1;
+            while(j>=0){
+                if(sides[j]==sides[i]) break;
+                j--;
+            }
+            if(j!=-1) continue;
+            //
+            
             sides[i]+=matchsticks[ind];
             if(dfs(matchsticks,sides,side,ind+1)) return true;
             sides[i]-=matchsticks[ind];
